7b5045cb12c65c9cb32ee50e4a0bc87571c94099
1004w455 <1004w455@gmail.com>
2017-06-04 01:17:41 +0900

    서버 시작 시간 보이기

--- src/main/java/com/example/test/controller/BaseController.java
+++ src/main/java/com/example/test/controller/BaseController.java
@@ -3,12 +3,24 @@ package com.example.test.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class BaseController {
 
-	@GetMapping("/")
-	public String root() {
-		return "success deploy";
-	}
+    private LocalDateTime startTime;
+
+    @PostConstruct
+    public void init() {
+        this.startTime = LocalDateTime.now();
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return String.format("deploy success (서버시작시간: %s)"
+                , startTime.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
+    }
 
 }
