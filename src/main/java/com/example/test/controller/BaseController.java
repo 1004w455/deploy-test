c8f729e4e3e4b2bc528df401768aeb592d559a88
1004w455 <1004w455@gmail.com>
2017-06-06 18:02:09 +0900

    호출된 시간 로그 쌓기

--- src/main/java/com/example/test/controller/BaseController.java
+++ src/main/java/com/example/test/controller/BaseController.java
@@ -19,6 +19,8 @@ public class BaseController {
 
     @GetMapping("/")
     public String root() {
+        System.out.println("호출됨: " +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
         return String.format("deploy success (서버시작시간: %s)"
                 , startTime.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
     }
