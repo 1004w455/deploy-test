1c3446eceb0cc53556d125b286d862a9ddf59e8b
1004w455 <1004w455@gmail.com>
2017-06-06 17:26:03 +0900

    서버 죽을때

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -3,10 +3,17 @@ package com.example.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    @PreDestroy
+    public void destroy() {
+        System.out.println("죽을때 이문자가 나와야함!!!");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 }
